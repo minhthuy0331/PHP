8c5e2d09df0685c82a1598334d23546cf6f24c99
Minh Thúy <96002309+minhthuy0331@users.noreply.github.com>
2022-07-13 15:58:28 +0700

    Task3

---
+++ Session01/DataType/DataTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Exmaple
+{
+    static void Main(string[] args)
+    {
+        int intVal;
+        double dblVal;
+        string strVal;
+        intVal = 10;
+        dblVal = 3.142;
+        strVal = "Fpt Aptech";
+        Console.WriteLine("{0} is an integer value", intVal);
+        Console.WriteLine("{0} is an double value", dblVal);
+        Console.WriteLine("{0} is an string", strVal);
+        Console.ReadLine();
+    }
+}
+
---
+++ Session01/NumberFormat/NumberFormat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class NumberFormat
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Currency formatting - {0:c} {1:C4}", 88.8, 888.8);
+        Console.WriteLine("Intefer formatting - {0:D5}", 88);
+        Console.WriteLine("Exponential formatting - {0:E}", 888);
+        Console.WriteLine("Fixed-point formatting - {0:F3}", 888.8888);
+        Console.WriteLine("General formatting - {0:G}" , 888.8888);
+        Console.WriteLine("Number formatting - {0:N}", 8888888.8);
+        Console.WriteLine("Hexadecinaml formatting - {0:X4}", 88);
+    }
+}
