775b2850f17b740fbcb4a16897f935168c9d47e6
Minh Thúy <96002309+minhthuy0331@users.noreply.github.com>
2022-07-18 16:46:56 +0700

    Task9

---
+++ Task9/GeometricObject/Circle.cs
@@ -0,0 +1,26 @@
+﻿public class Circle: GeometricObject
+{
+    private double radius;
+    public Circle(double x)
+    {
+        this.radius = x;
+    }
+    public Circle(double x, string c, double w) : base (c,w)
+    {
+        radius = x;
+    }
+    public override string ToString()
+    {
+        return "Circle has: radius is " + radius + 
+            ", color is" + PColor + ", weight is " + PWeight;
+
+    }
+    public override double findArea()
+    {
+        return Math.PI * radius * radius;
+    }
+    public override double findPerimeter()
+    {
+        return 2 * Math.PI * radius * radius;
+    }
+}
