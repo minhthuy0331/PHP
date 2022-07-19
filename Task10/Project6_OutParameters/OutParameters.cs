c1d1dec750d7af48a5860e368e246e8aa2fe24b1
Minh Thúy <96002309+minhthuy0331@users.noreply.github.com>
2022-07-19 16:50:36 +0700

    Task5

---
+++ Task10/Project6_OutParameters/OutParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Module6
+{
+    class OutParameters
+    {
+        static void Deprecitaion(out int val)
+        {
+            val = 20000;
+            int dep = val * 5 / 100;
+            int amt = val - dep;
+            Console.WriteLine("Depreciation Amount: " + dep);
+            Console.WriteLine("Reduced value after depreciation" + amt);
+
+        }
+        static void Main(string[] args)
+        {
+            int value;
+            Deprecitaion(out value);
+        }
+    }
+}
