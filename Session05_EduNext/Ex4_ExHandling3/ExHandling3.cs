13c8340971b24ea6f231350db66c67adf25d1c8d
Minh Thúy <96002309+minhthuy0331@users.noreply.github.com>
2022-07-25 16:12:15 +0700

    Session05_EduNext

---
+++ Session05_EduNext/Ex4_ExHandling3/ExHandling3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class MainClass
+{
+    public static int AnExceptionFunction(int value)
+    {
+        if (value == 0) // Can't divide by zezo
+            throw new DivideByZeroException("Divide By 0 error!");
+        int x = 20;
+        return x;
+    }
+    public static void Main()
+    {
+        int value = 0;
+        try
+        {
+            value = AnExceptionFunction(10);
+            Console.WriteLine("Value = {0}", value);
+            AnExceptionFunction(0);
+            Console.WriteLine("Value = {0}", value);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Caught an expection {0}. Continuing", e);
+        }
+        Console.WriteLine("Done");
+    }
+}
